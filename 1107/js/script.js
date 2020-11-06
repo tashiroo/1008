08cd38a92383f55db12e0735d38540be8085432f
taashiroo <ing631tom@outlook.jp>
2020-11-06 21:07:17 +0900

    11/06の学習内容

---
+++ 1107/js/script.js
@@ -0,0 +1,32 @@
+'use strict'
+
+{
+  function draw() {
+    const canvas = document.querySelector('canvas');
+    if(typeof canvas.getContext === 'undefined') {
+      return;
+    }
+     const ctx = canvas.getContext('2d');
+
+      ctx.beginPath();
+      ctx.moveTo(100,50);
+      ctx.lineTo(200,50);
+      ctx.setLineDash([5,10]);
+      ctx.stroke();
+     
+     
+      ctx.beginPath();
+      ctx.moveTo(100,100);
+      ctx.lineTo(200,100);
+      ctx.setLineDash([]);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(100,150);
+      ctx.lineTo(200,150);
+      ctx.lineWidth =16;
+      ctx.lineCap = `round`
+      ctx.stroke();
+    }
+  draw();
+}
